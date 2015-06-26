7d3705d7d9c5b9ee31eccb477ec0f9aeec11f404
PlasmaPower <ljbousfield@gmail.com>
2015-06-25 19:29:14 -0600

    Initial Commit

---
+++ download.sh
@@ -0,0 +1,6 @@
+CODE_LOCATION="https://raw.githubusercontent.com/PlasmaPower/SensorHub/master/code.ino"
+
+mkdir "$HOME/Desktop/SensorHub"
+cd "$HOME/Desktop/SensorHub"
+
+wget "$CODE_LOCATION" -O 'SensorHub.ino'
