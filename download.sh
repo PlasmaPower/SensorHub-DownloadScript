f085a130badb729c8b990be08cce274d8942993a
PlasmaPower <ljbousfield@gmail.com>
2015-06-25 19:36:33 -0600

    Added a diagram download

--- download.sh
+++ download.sh
@@ -1,6 +1,8 @@
 CODE_LOCATION="https://raw.githubusercontent.com/PlasmaPower/SensorHub/master/code.ino"
+DIAGRAM_LOCATION="https://docs.google.com/uc?authuser=0&id=0BwHVIM-U1GJMZW5tV09IbVNfeW8&export=download"
 
 mkdir "$HOME/Desktop/SensorHub"
 cd "$HOME/Desktop/SensorHub"
 
 wget "$CODE_LOCATION" -O 'SensorHub.ino'
+wget "$DIAGRAM_LOCATION" -O 'Diagram.png'
