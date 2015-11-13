8096210b6fbbb6087eb14990473bd3a5851fad93
PlasmaPower <ljbousfield@gmail.com>
2015-11-12 20:52:18 -0700

    Made the date more user friendly

--- download.sh
+++ download.sh
@@ -1,10 +1,10 @@
 CODE_LOCATION="https://raw.githubusercontent.com/PlasmaPower/SensorHub/master/code.ino"
 DIAGRAM_LOCATION="https://docs.google.com/uc?authuser=0&id=0BwHVIM-U1GJMZW5tV09IbVNfeW8&export=download"
 
-date=$(date '+%m-%e-%y-%H:%M')
+date=$(date '+%B %d %Y (%H:%M)')
 
-mkdir "$HOME/Desktop/SensorHub-$date"
-cd "$HOME/Desktop/SensorHub-$date"
+mkdir "$HOME/Desktop/SensorHub $date"
+cd "$HOME/Desktop/SensorHub $date"
 
 curl -L "$CODE_LOCATION" -o 'SensorHub.ino'
 curl -L "$DIAGRAM_LOCATION" -o 'Diagram.png'
