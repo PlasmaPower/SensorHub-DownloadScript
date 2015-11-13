f1b14edf25d64b1e0850eb87c7a914d41934fd55
PlasmaPower <ljbousfield@gmail.com>
2015-11-12 20:49:20 -0700

    Made the SensorHub folder name date dependent

--- download.sh
+++ download.sh
@@ -1,8 +1,10 @@
 CODE_LOCATION="https://raw.githubusercontent.com/PlasmaPower/SensorHub/master/code.ino"
 DIAGRAM_LOCATION="https://docs.google.com/uc?authuser=0&id=0BwHVIM-U1GJMZW5tV09IbVNfeW8&export=download"
 
-mkdir "$HOME/Desktop/SensorHub"
-cd "$HOME/Desktop/SensorHub"
+date=$(date '+%m-%e-%y-%H:%M')
+
+mkdir "$HOME/Desktop/SensorHub-$date"
+cd "$HOME/Desktop/SensorHub-$date"
 
 curl -L "$CODE_LOCATION" -o 'SensorHub.ino'
 curl -L "$DIAGRAM_LOCATION" -o 'Diagram.png'
