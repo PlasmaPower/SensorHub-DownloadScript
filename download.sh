d15245832ed43ae220f32a6dea3e05c3e33a10a0
PlasmaPower <ljbousfield@gmail.com>
2016-06-02 13:34:49 -0600

    Update code.ino to SensorHub.ino

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-CODE_LOCATION="https://raw.githubusercontent.com/PlasmaPower/SensorHub/master/code.ino"
+CODE_LOCATION="https://raw.githubusercontent.com/PlasmaPower/SensorHub/master/SensorHub.ino"
 DIAGRAM_LOCATION="https://docs.google.com/uc?authuser=0&id=0BwHVIM-U1GJMZW5tV09IbVNfeW8&export=download"
 
 date=$(date '+%B %d %Y (%H:%M)')
